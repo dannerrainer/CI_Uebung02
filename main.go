ff6e7f78af66929f856da1016b5650c5138abe9e
Danner Rainer <rainer656565@gmail.com>
2022-05-25 15:41:49 +0200

    Go workflow: Changed to env variables again

--- main.go
+++ main.go
@@ -4,15 +4,10 @@ import "os"
 
 func main() {
 	a := App{}
-	/*a.Initialize(
-	os.Getenv("APP_DB_USERNAME"),
-	os.Getenv("APP_DB_PASSWORD"),
-	os.Getenv("APP_DB_NAME"))
-	*/
 	a.Initialize(
-		os.Getenv("postgres"),
-		os.Getenv("postgres"),
-		os.Getenv("ci_uebung02"))
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	a.Run(":8010")
 }
